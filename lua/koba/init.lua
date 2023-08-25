ac5ca4b012df4dfa33648fc0af5b1c9c2577f5d7
Arman <persakiss@gmail.com>
2023-08-25 00:25:58 -0400

    moved autoclose to after/, works

--- lua/koba/init.lua
+++ lua/koba/init.lua
@@ -1,6 +1,5 @@
 require("koba.remap")
 require("koba.set")
-require("autoclose").setup()
 
 
 
--- lua/koba/packer.lua
+++ lua/koba/packer.lua
@@ -57,7 +57,15 @@ return require('packer').startup(function(use)
   use 'gko/vim-coloresque'
 
   use 'SirVer/ultisnips'
-  use 'honza/vim-snippets'
   use 'mlaursen/vim-react-snippets'
   use 'm4xshen/autoclose.nvim'
+
+  use({
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!:).
+    run = "make install_jsregexp"
+  })
+  use "rafamadriz/friendly-snippets"
 end)
