0ba079e8d796ba2cd50dbafb21824942314709b7
Arman <persakiss@gmail.com>
2023-08-25 01:21:32 -0400

    remap for snippets

--- lua/koba/init.lua
+++ lua/koba/init.lua
@@ -2,16 +2,14 @@ require("koba.remap")
 require("koba.set")
 
 
--- local luasnip = require("luasnip")
---
--- luasnip.filetype_extend("javascript", {"html"})
--- luasnip.filetype_extend("javascriptreact", {"html"})
--- luasnip.filetype_extend("typescriptreact", {"html"})
---
--- require("luasnip/loaders/from_vscode").lazy_load()
+local luasnip = require("luasnip")
 
+-- Extend typescriptreact with snippets from html, javascript, and typescript
+luasnip.filetype_extend("typescriptreact", {"html", "javascript", "typescript"})
 
+luasnip.filetype_extend("javascript", {"html"})
+luasnip.filetype_extend("javascriptreact", {"html"})
+luasnip.filetype_extend("typescriptreact", {"html"})
 
-
-
+require("luasnip/loaders/from_vscode").lazy_load()
 
--- lua/nvim_cmp_config.lua
+++ lua/nvim_cmp_config.lua
@@ -4,13 +4,19 @@ local cmp_action = require('lsp-zero').cmp_action()
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
   sources = {
-    {name = 'nvim_lsp'},
-    {name = 'luasnip'},
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
   },
   mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
   }
 })
-
