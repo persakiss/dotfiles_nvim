5b3ecb8848ea8b25a9696623a82ac93a29707f98
Arman <persakiss@gmail.com>
2023-09-16 16:39:32 -0400

    not sure if its picking up ultisnips, but it's working.

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -13,6 +13,21 @@ luasnip.filetype_extend("javascript", {"html"})
 luasnip.filetype_extend("javascriptreact", {"html"})
 luasnip.filetype_extend("typescriptreact", {"html"})
  
+vim.g['luasnip.snippet_dirs'] = {'~/.config/nvim/snipbook'}
 
+vim.api.nvim_set_keymap('i', '<c-l>', '<cmd>lua require("luasnip").expand_or_jump()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<c-h>', '<cmd>lua require("luasnip").jump(-1)<CR>', { noremap = true, silent = true })
+
+-- Tab functionality to either expand LuaSnip snippet or fallback to UltiSnips
+vim.api.nvim_set_keymap('i', '<Tab>', 'v:lua.expand_snippet_or_fallback()', { noremap = true, expr = true, silent = true })
+
+
+-- Lua function to expand snippet or fallback to UltiSnips
+_G.expand_snippet_or_fallback = function()
+  if not require("luasnip").expand() then
+    vim.fn["UltiSnips#ExpandSnippetOrJump"]()
+  end
+  return ""
+end
 
 require("luasnip/loaders/from_vscode").lazy_load()
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -258,30 +258,30 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: ultisnips
-time([[Config for ultisnips]], true)
-try_loadstring("\27LJ\2\nŸ\1\0\0\2\0\a\0\r6\0\0\0009\0\1\0'\1\3\0=\1\2\0006\0\0\0009\0\1\0'\1\3\0=\1\4\0006\0\0\0009\0\1\0'\1\6\0=\1\5\0K\0\1\0\f<s-tab>!UltiSnipsJumpBackwardTrigger UltiSnipsJumpForwardTrigger\n<tab>\27UltiSnipsExpandTrigger\6g\bvim\0", "config", "ultisnips")
-time([[Config for ultisnips]], false)
--- Config for: tex-conceal.vim
-time([[Config for tex-conceal.vim]], true)
-try_loadstring("\27LJ\2\n‚\1\0\0\3\0\a\0\r6\0\0\0009\0\1\0'\2\2\0B\0\2\0016\0\0\0009\0\3\0'\1\5\0=\1\4\0006\0\0\0009\0\1\0'\2\6\0B\0\2\1K\0\1\0\28hi Conceal ctermbg=none\nabdmg\16tex_conceal\6g\23set conceallevel=1\bcmd\bvim\0", "config", "tex-conceal.vim")
-time([[Config for tex-conceal.vim]], false)
--- Config for: live-server.nvim
-time([[Config for live-server.nvim]], true)
-require("live-server").setup()
-time([[Config for live-server.nvim]], false)
--- Config for: rose-pine
-time([[Config for rose-pine]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
-time([[Config for rose-pine]], false)
 -- Config for: auto-save.nvim
 time([[Config for auto-save.nvim]], true)
 try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14auto-save\frequire\0", "config", "auto-save.nvim")
 time([[Config for auto-save.nvim]], false)
+-- Config for: ultisnips
+time([[Config for ultisnips]], true)
+try_loadstring("\27LJ\2\nŸ\1\0\0\2\0\a\0\r6\0\0\0009\0\1\0'\1\3\0=\1\2\0006\0\0\0009\0\1\0'\1\3\0=\1\4\0006\0\0\0009\0\1\0'\1\6\0=\1\5\0K\0\1\0\f<s-tab>!UltiSnipsJumpBackwardTrigger UltiSnipsJumpForwardTrigger\n<tab>\27UltiSnipsExpandTrigger\6g\bvim\0", "config", "ultisnips")
+time([[Config for ultisnips]], false)
 -- Config for: vimtex
 time([[Config for vimtex]], true)
 try_loadstring("\27LJ\2\n‚\1\0\0\2\0\a\0\r6\0\0\0009\0\1\0'\1\3\0=\1\2\0006\0\0\0009\0\1\0'\1\5\0=\1\4\0006\0\0\0009\0\1\0)\1\0\0=\1\6\0K\0\1\0\25vimtex_quickfix_mode\fzathura\23vimtex_view_method\nlatex\15tex_flavor\6g\bvim\0", "config", "vimtex")
 time([[Config for vimtex]], false)
+-- Config for: live-server.nvim
+time([[Config for live-server.nvim]], true)
+require("live-server").setup()
+time([[Config for live-server.nvim]], false)
+-- Config for: tex-conceal.vim
+time([[Config for tex-conceal.vim]], true)
+try_loadstring("\27LJ\2\n‚\1\0\0\3\0\a\0\r6\0\0\0009\0\1\0'\2\2\0B\0\2\0016\0\0\0009\0\3\0'\1\5\0=\1\4\0006\0\0\0009\0\1\0'\2\6\0B\0\2\1K\0\1\0\28hi Conceal ctermbg=none\nabdmg\16tex_conceal\6g\23set conceallevel=1\bcmd\bvim\0", "config", "tex-conceal.vim")
+time([[Config for tex-conceal.vim]], false)
+-- Config for: rose-pine
+time([[Config for rose-pine]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
+time([[Config for rose-pine]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
